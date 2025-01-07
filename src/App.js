f3aeb81582d64a09cb2d26bef3173df13350e045
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-01-07 22:44:12 +0530

    First custom component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
       <h1>Lets get Started</h1>
       <p>I am learning React</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem()
+{
+return(
+    <div>
+        <div>food-$10</div>
+        <div>movie-$20</div>
+    </div>
+)
+}
+export default ExpenseItem;
